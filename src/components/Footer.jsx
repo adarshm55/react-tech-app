7391b002bb6eb5a2739753e27663339c0340ee6a
Adarsh Mishra <87515095+adarshm55@users.noreply.github.com>
2023-06-11 14:54:58 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://instagram.com/adarsh.m__55" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/adarshm55" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
